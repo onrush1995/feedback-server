83e1cc420c5d393315a2eedb15dff1e2188c70a5
Candice Davidson <cndragn@gmail.com>
2018-11-24 23:14:26 -0800

    prepare for heroku deployment

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.get('/', (req, res) => {
 	res.send({ hi: 'there' });
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
 
 // run and test app
 // node index.js
