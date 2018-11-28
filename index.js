8ce9f61ac48c4894388f3b1b1499afb8c9640236
Candice Davidson <cndragn@gmail.com>
2018-11-28 00:26:47 -0800

    setup cookies to use for auth

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User'); // require models before passport
 require('./services/passport');
@@ -7,6 +9,19 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
+
+//tell express to make use of cookies
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+		//key to encrypt cookie
+		keys: [ keys.cookieKey ]
+	})
+);
+
+//tell passport to use cookies to handle auth
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
