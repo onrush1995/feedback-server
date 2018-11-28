ff763b9d01936a33e312c0a041e6f1acf7803d27
Candice Davidson <cndragn@gmail.com>
2018-11-27 22:46:52 -0800

    create user model class

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 require('./services/passport');
+require('./models/User');
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 
-// const authRoutes = require('./routes/authRoutes');
-// authRoutes(app);
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
