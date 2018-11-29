9daadc35684f7c2f18c889be9303d85aff414c59
Candice Davidson <cndragn@gmail.com>
2018-11-29 01:43:24 -0800

    update prod variable causing oauth error

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 // production keys
 module.exports = {
-	googleClientID: 'process.env.GOOGLE_CLIENT_ID',
-	googleClientSecret: 'process.env.GOOGLE_CLIENT_SECRET',
-	mongoURI: 'process.env.MONGO_URI',
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	mongoURI: process.env.MONGO_URI,
 	// make up anything for cookieKey
-	cookieKey: 'process.env.COOKE_KEY'
+	cookieKey: process.env.COOKE_KEY
 };
