aa2f1a3f7a925a5ecc80c4e554e5dc233df75ec4
Candice Davidson <cndragn@gmail.com>
2018-11-29 02:24:17 -0800

    fix blank current_user view in prod

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,6 @@
-// production keys
 module.exports = {
 	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	mongoURI: process.env.MONGO_URI,
-	// make up anything for cookieKey
 	cookieKey: process.env.COOKE_KEY
 };
