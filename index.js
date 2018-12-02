aee0f0d9f2df7921562ca20d3bf1634d5c29fabc
Candice Davidson <cndragn@gmail.com>
2018-12-02 14:11:42 -0800

    configure routing to prepare react for heroku deployment

--- index.js
+++ index.js
@@ -25,5 +25,18 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+	// Express will serve up production assets
+	// like main.js or main.css
+	app.use(express.static('client/build'));
+
+	// Express will serve up index.html
+	// if it doesnt recognize the route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
