fc695b55ed22fe84159db310f5f1d1083c8caa29
Candice Davidson <cndragn@gmail.com>
2018-12-02 22:58:31 -0800

    create subdocument collection for recipients

--- models/Survey.js
+++ models/Survey.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
 	title: String,
 	body: String,
 	subject: String,
-	recipients: [ String ],
+	// create Recipient subDocument Collection to track
+	// whether a recipient already responded
+	recipients: [ RecipientSchema ],
 	yes: { type: Number, default: 0 },
 	no: { type: Number, default: 0 }
 });
