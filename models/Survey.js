a764bf43e84bee29c84c0a9b7940fb3817274dfd
Candice Davidson <cndragn@gmail.com>
2018-12-02 23:03:56 -0800

    setup relationship btwn user and survey

--- models/Survey.js
+++ models/Survey.js
@@ -10,7 +10,10 @@ const surveySchema = new Schema({
 	// whether a recipient already responded
 	recipients: [ RecipientSchema ],
 	yes: { type: Number, default: 0 },
-	no: { type: Number, default: 0 }
+	no: { type: Number, default: 0 },
+	// connect survey to a particular user
+	// underscore makes it obv its setting up a relationship
+	_user: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
 mongoose.model('surveys', surveySchema);
