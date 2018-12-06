b621e3bc697b7360983400055f8a4972cf84bdaa
Candice Davidson <cndragn@gmail.com>
2018-12-05 22:22:24 -0800

    register body with email and enable click tracking

--- services/Mailer.js
+++ services/Mailer.js
@@ -10,12 +10,26 @@ class Mailer extends helper.Mail {
 		this.subject = subject;
 		this.body = new helper.Content('text/html', content);
 		this.recipients = this.formatAddresses(recipients);
+
+		//register body with email itself
+		this.addContent(this.body);
+
+		//enable click tracking inside of email
+		this.addClickTracking();
 	}
 
 	formatAddresses(recipients) {
 		return recipients.map(({ email }) => {
 			return new helper.EmailAddress(email);
 		});
+	}
+
+	addClickTracking() {
+		const trackingSettings = new helper.TrackingSettings();
+		const clickTracking = new helper.ClickTracking(true, true);
+
+		trackingSettings.setClickTracking(clickTracking);
+		this.addTrackingSettings(trackingSettings);
 	}
 }
 
