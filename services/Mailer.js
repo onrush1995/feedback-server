232c8bf45f020748d76fe52b83f5775898acb917
Candice Davidson <cndragn@gmail.com>
2018-12-05 22:16:10 -0800

    create helper function to iterate through email addresses

--- services/Mailer.js
+++ services/Mailer.js
@@ -5,10 +5,17 @@ const keys = require('../config/keys');
 class Mailer extends helper.Mail {
 	constructor({ subject, recipients }, content) {
 		super();
+
 		this.from_email = new helper.EmailAddress('no-reply@candicedavidson.com');
 		this.subject = subject;
 		this.body = new helper.Content('text/html', content);
 		this.recipients = this.formatAddresses(recipients);
+	}
+
+	formatAddresses(recipients) {
+		return recipients.map(({ email }) => {
+			return new helper.EmailAddress(email);
+		});
 	}
 }
 
